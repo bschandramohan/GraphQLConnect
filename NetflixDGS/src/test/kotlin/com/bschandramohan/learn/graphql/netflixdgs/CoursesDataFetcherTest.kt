9da55bb3ee2c5c041b28c607542d96ad404042d7
bschandramohan <bschandramohan@gmail.com>
2021-12-14 01:22:13 -0800

    Fixing tests to make sure all dependent classes are included Adding Test for query by parameter

--- NetflixDGS/src/test/kotlin/com/bschandramohan/learn/graphql/netflixdgs/CoursesDataFetcherTest.kt
+++ NetflixDGS/src/test/kotlin/com/bschandramohan/learn/graphql/netflixdgs/CoursesDataFetcherTest.kt
@@ -1,16 +1,16 @@
 package com.bschandramohan.learn.graphql.netflixdgs
 
-import com.bschandramohan.learn.graphql.netflixdgs.datafetchers.CoursesDataFetcher
+import com.bschandramohan.learn.graphql.netflixdgs.client.CoursesGraphQLQuery
+import com.bschandramohan.learn.graphql.netflixdgs.client.CoursesProjectionRoot
 import com.netflix.graphql.dgs.DgsQueryExecutor
-import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration
+import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(classes = [DgsAutoConfiguration::class, CoursesDataFetcher::class])
+@SpringBootTest
 class CoursesDataFetcherTest {
-
     @Autowired
     lateinit var dgsQueryExecutor: DgsQueryExecutor
 
@@ -28,5 +28,23 @@ class CoursesDataFetcherTest {
         )
 
         assertThat(titles.size).isEqualTo(2)
+    }
+
+    @Test
+    fun coursesQuery() {
+        val graphQLQueryRequest = GraphQLQueryRequest(
+            CoursesGraphQLQuery
+                .Builder()
+                .courseTitle("Amazing Python")
+                .build(),
+            CoursesProjectionRoot().title()
+        )
+
+        val titles: List<String> = dgsQueryExecutor.executeAndExtractJsonPath(
+            graphQLQueryRequest.serialize(),
+            "data.courses[*].title"
+        )
+
+        assertThat(titles.size == 1)
     }
 }
